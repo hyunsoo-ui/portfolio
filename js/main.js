72947502680779dbaf7786e1fe1b6c6b0f632fb0
hyunsoo-ui <bugy1234@naver.com>
2021-08-07 15:10:46 +0900

    첫번째 커밋

---
+++ js/main.js
@@ -0,0 +1,36 @@
+(function() {
+
+
+    var workSlider = new Swiper('.work_slider', {
+        // loop: true,
+        slidesPerView: 1,
+        // spaceBetween: 10,
+        // pagination: {
+        //   el: '.swiper-pagination',
+        //   type:'fraction',
+        // },
+      
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+      
+    });
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
